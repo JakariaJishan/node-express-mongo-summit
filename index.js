c540dc30458f194329846072e32360bfbf5163af
JakariaJishan <jakariajishan987@gmail.com>
2022-04-05 11:49:54 +0600

    fs

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-let _ = require('lodash');
-console.log(_.last([2,3,4]));
+// let _ = require('lodash');
+// console.log(_.last([2,3,4]));
+
+let fs = require('fs');
+
+// fs.writeFileSync('demo.txt','hello world');
+// fs.appendFileSync('demo.txt',' this is jack');
+
+fs.readFile('demo.txt', (err, data) => {
+    console.log(data.toString());
+})
